323edd37ced0d7e85dac1df93a5872898d403a0d
jvgua <jvgualdi1@gmail.com>
2025-06-23 18:10:22 -0300

    Ajuste de listener mensageria

--- src/main/java/jvgualdi/tec/wallet/microservice/consumers/OrderEventListener.java
+++ src/main/java/jvgualdi/tec/wallet/microservice/consumers/OrderEventListener.java
@@ -1,9 +1,10 @@
-package jvgualdi.tec.wallet.microservice.events;
+package jvgualdi.tec.wallet.microservice.consumers;
 
 import jvgualdi.tec.wallet.microservice.dto.OrderCreatedEvent;
 import jvgualdi.tec.wallet.microservice.service.WalletService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,8 +13,8 @@ public class OrderEventListener {
 
     private final WalletService walletService;
 
-    @RabbitListener(queues = "order.created")
-    public void onOrderCreated(OrderCreatedEvent evt) {
+    @RabbitListener(queues = "{spring.rabbitmq.queue}")
+    public void onOrderCreated(@Payload OrderCreatedEvent evt) {
         walletService.debit(evt.customerId(), evt.total(), evt.orderId());
     }
 }
